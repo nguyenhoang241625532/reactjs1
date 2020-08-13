fea6f129a627085d2aff5bb15deeec84e9a2fa17
nguyenhoang241625532 <49204135+nguyenhoang241625532@users.noreply.github.com>
2020-08-13 17:38:11 +0700

    https://github.com/nguyenhoang241625532/reactjs1.git

---
+++ precache-manifest.31f527a5c41e33dacc701fe4bce42c77.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ac296cbca90fac91cc0c5737a9743de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d938954a0ceaf3151cba",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "b4e5dd3c16d05e42a1e4",
+    "url": "/static/css/main.19910c4d.chunk.css"
+  },
+  {
+    "revision": "d938954a0ceaf3151cba",
+    "url": "/static/js/2.320757a7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.320757a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4e5dd3c16d05e42a1e4",
+    "url": "/static/js/main.fdd98c63.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "5c9d996f9882946f273ef7b40f623cc8",
+    "url": "/static/media/0_01_13.5c9d996f.jpg"
+  },
+  {
+    "revision": "88c7f8e212d6859ea7a2d8231df31570",
+    "url": "/static/media/2-1594950196702-1-250x250.88c7f8e2.jpg"
+  },
+  {
+    "revision": "dd45498aa08ba5a57e0ebf3c808121bf",
+    "url": "/static/media/abc.dd45498a.png"
+  },
+  {
+    "revision": "06974df2633a064b4bb01ca4df7402db",
+    "url": "/static/media/anh-buon-ve-dem-5-1280x800-600x400.06974df2.jpg"
+  },
+  {
+    "revision": "9351fda16d661ece810f4cc6f841235b",
+    "url": "/static/media/cach-lam-banh-mochi-2.9351fda1.png"
+  },
+  {
+    "revision": "e761473b72810ae406f5b98b671b70c5",
+    "url": "/static/media/cach-nau-ca-ri-ga-5-600x400.e761473b.jpg"
+  },
+  {
+    "revision": "95240ba55951c25ad936188377f38cda",
+    "url": "/static/media/cach-nau-lau-thai-200x150.95240ba5.gif"
+  },
+  {
+    "revision": "15b95bf880594c2dcebff34e4c5d90ff",
+    "url": "/static/media/chay-no-1595299555124-1.15b95bf8.jpg"
+  },
+  {
+    "revision": "b8e48f1a53be6cf39c43e3d156a8f85d",
+    "url": "/static/media/chay-no-1595299555124-2-250x250.b8e48f1a.jpg"
+  },
+  {
+    "revision": "25e879776d8d263504aea1feb28b67c8",
+    "url": "/static/media/chum-tour-du-lich-dong-tay-bac-mixtourist-3-600x400.25e87977.jpg"
+  },
+  {
+    "revision": "eb5fc1b3f0ada3238d05960f6df52f1e",
+    "url": "/static/media/cuoc-song-doi-thuong-voi-nhung-hinh-anh-gian-di-nhung-vo-cung-dep-6-600x400.eb5fc1b3.jpg"
+  },
+  {
+    "revision": "e7867ec9a06e7b00bc3bd4370de3ac10",
+    "url": "/static/media/download-12-195x194.e7867ec9.jpg"
+  },
+  {
+    "revision": "a5a275918b1fa14c2adb25b810c0ea15",
+    "url": "/static/media/hinh-anh-hai-huoc-fb-3-600x400.a5a27591.jpg"
+  },
+  {
+    "revision": "85534d8b5660366d8e5931aa66e90197",
+    "url": "/static/media/hinh-anh-hoat-hinh-dep-nhat-2-600x363.85534d8b.jpg"
+  },
+  {
+    "revision": "c07967ca04cd940be5c5c586607b7e60",
+    "url": "/static/media/hinh-anh-mua-he-dep-9-1-600x400.c07967ca.jpg"
+  },
+  {
+    "revision": "8c8e8006e93a17853a02b077cdf7b1e6",
+    "url": "/static/media/hinh-anh-xin-loi-nguoi-yeu-ngo-nghinh_095942919-1067x800-600x400.8c8e8006.jpg"
+  },
+  {
+    "revision": "8a2d91693bb45ff7268eb7fbfaa2f228",
+    "url": "/static/media/hinh-nen-may-tinh-de-thuong-65-600x400.8a2d9169.jpg"
+  },
+  {
+    "revision": "a2aa27100fcd8e899228eddd068e2ef1",
+    "url": "/static/media/hinh-nen-tinh-yeu-lang-man-13-3-600x400.a2aa2710.jpg"
+  },
+  {
+    "revision": "75df087bd6d0c4f75d8a8361e546b2d1",
+    "url": "/static/media/hung_long_2-200x150.75df087b.jpg"
+  },
+  {
+    "revision": "7103d1040b921b32cf9d12e98aacc443",
+    "url": "/static/media/huong-dan-lam-mut-dua-ngon-19-195x345.7103d104.jpg"
+  },
+  {
+    "revision": "553c0845591682a40845f19295a96de5",
+    "url": "/static/media/huong-dan-lam-mut-dua-ngon-19-200x150.553c0845.jpg"
+  },
+  {
+    "revision": "6cb0d095c403dd06723b4da52ce19436",
+    "url": "/static/media/idcpr-dantri.6cb0d095.png"
+  },
+  {
+    "revision": "a0f662ea8a4954e812183925f1b3f7ec",
+    "url": "/static/media/laptop-1596500315363-2-250x250.a0f662ea.jpg"
+  },
+  {
+    "revision": "5d38d5cae671c1fe780b402e31484096",
+    "url": "/static/media/lau-rieu-cua-bap-bo-200x150.5d38d5ca.jpg"
+  },
+  {
+    "revision": "fab656169dcfb9ac26f6586c7c89f76b",
+    "url": "/static/media/phimmoi-01592536084248-1595357487503.fab65616.png"
+  },
+  {
+    "revision": "751ffb4b05137d8c988453e2c1ad1d6d",
+    "url": "/static/media/s-4-reutersmedianet-1596525479951-2-250x250.751ffb4b.jpg"
+  },
+  {
+    "revision": "3437176da49944f343df778fecee0ef6",
+    "url": "/static/media/sua-fianldocx-1595773291623-250x250.3437176d.jpeg"
+  },
+  {
+    "revision": "4b52d1164955fbd5160e3a33ebbd506c",
+    "url": "/static/media/suon-kho-ngu-sac-195x345.4b52d116.jpg"
+  },
+  {
+    "revision": "9b056919b323fc0a9d986cea8fcca88b",
+    "url": "/static/media/suon-kho-ngu-sac-200x150.9b056919.jpg"
+  },
+  {
+    "revision": "45b082fe4e8352f71094f8be73855641",
+    "url": "/static/media/toc-do-internet-av-1595584485348-250x250.45b082fe.jpg"
+  },
+  {
+    "revision": "01c2b4a37cec44dca349a1f9e0d86e06",
+    "url": "/static/media/twitter-hack-1595288929244.01c2b4a3.jpg"
+  },
+  {
+    "revision": "d0ae725eb6cc6a168b9d8bcccd5b58e1",
+    "url": "/static/media/tình-thơ-người-lính-hải-đảo-1-599x400-599x400.d0ae725e.jpg"
+  },
+  {
+    "revision": "6f832b51eaeff9ebe646d107b618a3d0",
+    "url": "/static/media/vkool-1-1595357627181-2-250x250.6f832b51.jpg"
+  }
+]);
